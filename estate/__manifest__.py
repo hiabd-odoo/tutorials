de3c518f05dfe33c7f98c5a20eb7253bf5877a20
hiabd-odoo <hiabd@odoo.com>
2026-01-20 11:08:49 +0400

    Chapter 5: Add ui

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,5 +7,8 @@
     'description': 'View property listings and offers.',
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
+    'application': True,
 }
