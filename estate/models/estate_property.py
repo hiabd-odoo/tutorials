cc3059f6ab9f276343b4f9c7b2e4ef9836c03b54
hiabd-odoo <hiabd@odoo.com>
2026-01-21 17:43:38 +0400

    Chapter 6+7: Basic Views + Relations + Refactoring

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -2,42 +2,37 @@ from odoo import fields, models
 from dateutil.relativedelta import relativedelta
 
 class EstateProperty(models.Model):
-    _name = "estate_property"
-    _description = "estate_property"
-    _postcode = "estate_property"
-    _date_availability = "estate_property"
-    _expected_price = "estate_property"
-    _selling_price = "estate_property"
-    _bedrooms = "estate_property"
-    _living_area = "estate_property"
-    _facades = "estate_property"
-    _garage = "estate_property"
-    _garden = "estate_property"
-    _garden_area = "estate_property"
-    _garden_orientation = "estate_property"
-    
+    _name = "estate.property"
+    _description = "Estate Property"
 
-    name = fields.Char(required=True)
+
+    name = fields.Char(required=True,string="Title")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False,default=fields.Date.today()+relativedelta(months=3))
+    date_availability = fields.Date(copy=False,default=fields.Date.today()+relativedelta(months=3),string='Available From')
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True,copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientation = fields.Selection(string='Garden Orientation',selection=[('north','North'),('south','South'),('east','East'),('west','West')])
+    hasGarage = fields.Boolean(string="Garage")
+    hasGarden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(selection=[('north','North'),('south','South'),('east','East'),('west','West')])
     active = fields.Boolean('Active', default=True)
     state = fields.Selection(selection=[
             ('new','New'),
-            ('offer received','Offer Received'),
-            ('offer accepted','Offer Accepted'),
+            ('offerReceived','Offer Received'),
+            ('offerAccepted','Offer Accepted'),
             ('sold','Sold'),
             ('cancelled','Cancelled')], 
         default='new',
         required=True,
         copy=False)
+    property_type_id = fields.Many2one("estate.property.type")
+    buyer_id = fields.Many2one("res.partner",copy=False)
+    salesperson_id = fields.Many2one("res.users",default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag")
+    offer_ids = fields.One2many("estate.property.offer","property_id")
+
     
