2488b92b13ea80c869e9933bf8199d492bcadd5c
hiabd-odoo <hiabd@odoo.com>
2026-01-28 11:46:29 +0400

    Code reviewed: append to vals_list

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import _, models
 from odoo.orm.commands import Command
 
 
@@ -6,8 +6,12 @@ class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def sell_property(self):
+        super().sell_property()
+
+        vals_list = []
+
         for record in self:
-            self.env["account.move"].create([
+            vals_list.append(
                 {
                     "partner_id":record.buyer_id.id,
                     "move_type":"out_invoice",
@@ -23,5 +27,16 @@ class EstateProperty(models.Model):
                             "price_unit":100,
                         }),
                     ]
-                }])
-        return super().sell_property()
+                })
+        if vals_list:
+            invoices = self.env["account.move"].create(vals_list)
+            return {
+                "type": "ir.actions.act_window",
+                "res_model": "account.move",
+                "name": _("Invoices"),
+                "views": [[False, 'form']] if len(invoices) == 1 else [[False, "list"], [False, "form"]],
+                "context": {"create": False},
+                "domain": [('id', 'in', invoices.ids)],
+                "res_id": invoices.id if len(invoices) == 1 else False,
+            }
+        return True
